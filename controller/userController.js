3d0e862922c4afa2bf343fdbf8c768ae723e38b5
vindhyaveera <vindhyadevi12@gmail.com>
2024-11-25 14:02:19 +0530

    youtube

--- controller/userController.js
+++ controller/userController.js
@@ -210,7 +210,7 @@ const getuserVideos = async function (req, res) {
       include: [
         {
           model: model.BigVideos, // Include associated BigVideos
-          as: "bigvideos", // Use the correct alias from your associations
+          as: "Bigvideosuser", // Use the correct alias from your associations
         },
       ],
     });
